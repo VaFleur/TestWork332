afeffc5600fe55c1f841b90084de80c9f582bb53
Anton Zhilinskey <zhilinskiianton@gmail.com>
2025-05-08 15:11:49 +0700

    Added security.py

---
+++ app/security.py
@@ -0,0 +1,13 @@
+from fastapi import HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+api_key_header = APIKeyHeader(name="Authorization", auto_error=False)
+
+async def get_api_key(api_key: str = Security(api_key_header)):
+    if api_key != f"ApiKey {API_KEY}":
+        raise HTTPException(status_code=403, detail="Invalid API Key")
